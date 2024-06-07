5ba358f38102d80e30c5207e90228615a98a4e32
CyprianRachel <cyprianrachel@gmail.com>
2024-06-07 20:39:42 +0200

    Conditional Rendering

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,23 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { Link } from "./components/Link";
-import { HeadingItalic } from "./components/HeadingItalic";
-import { Logger } from "./components/Logger";
+import App from "./App";
 
-const element = (
-  <>
-    <Logger>
-      <HeadingItalic>Nasza super strona</HeadingItalic>
-    </Logger>
-    <main>
-      <Link text="Sprawdź stronę kursu!" shouldOpenNewTab={true} />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere labore
-        blanditiis magnam eveniet rem ratione, voluptatum nulla inventore
-        quaerat necessitatibus.
-      </p>
-      <Link shouldOpenNewTab={false} />
-    </main>
-  </>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
